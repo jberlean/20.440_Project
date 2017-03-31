12659164bde633faf26225850bd083f1bda3c0fa
PatrickHolec <abc123@gmail.com>
2017-03-30 21:19:15 -0400

    patrick first commit

--- dry_estimator.py
+++ dry_estimator.py
@@ -1,60 +1,140 @@
+
+'''
+Project (working title): 
+    Multicell sequence decoupling
+Function: 
+    Simulates the ability to decouple information about a population of paired chain sequences using combinatorics
+Purpose: 
+    Joseph and Patrick 440 project
+'''
+
+'''
+Library imports and initialization
+'''
+
+# standard libraries
 import operator as op
+
+# nonstandard libraries
 import matplotlib.pyplot as plt
 import numpy as np
 
+'''
+Main function execution (if file not imported, this will run)
+'''
 
-def ncr(n, r):
+def main():
+    mcs = Multicell_Sequencing(w=1000,n=100)
+    mcs.build_repertoire(alpha=1.0,count=20000)
+    mcs.fs_logspace(-9,-1,40)
+    fs,results = mcs.start_test()
+
+    print 'Clone repertoires :',mcs.p[:10]
+    print 'Clone repertoires:',mcs.p[-10:]
+
+    visualize_1D(fs,results)
+    visualize_repertoire(mcs.p) 
+
+'''
+Factory methods
+'''
+
+# N choose K evaluation, with approximated results for N >> 1
+def ncr_exact(n, r):
     r = min(r, n-r)
     if r == 0: return 1
     numer = reduce(op.mul, xrange(n, n-r, -1))
     denom = reduce(op.mul, xrange(1, r+1))
     return numer//denom
 
+# approximation of binomial distribution
+def ncr_approx(n, r, f):
+    avg,var = n*f,n*f*(1-f)
+    return (1/((2*var*np.pi)**0.5))*np.exp(-((r-avg)**2)/(2*var))
 
-w = 1000  # number of wells
-n = 35  # number of cells/well
-fs = [10**(-f/5.) for f in xrange(2,31)]
-#fs = [float(f)/2000 for f in xrange(1,21)]
-#fs = [.05]
-alpha = 1
+# creates a series of numbers representing binomial distribution (approx. and exact)
+def binomial_list(w,f):
+    if False: #w*f > 50 and w*(1-f) > 50: # condition for inaccurate approximation
+        temp = [ncr_approx(n,i,f) for i in xrange(0,w+1)]
+        return [t/sum(temp) for t in temp]
+    else:
+        return [ncr_exact(w,i)*(f**i)*((1-f)**(w-i)) for i in xrange(0,w+1)]
 
-# generate distribution
-occurs = xrange(1,20001)
-p = [o**-alpha for o in occurs]
-p = [i/sum(p) for i in p]
-p_present = [1 - (1-i)**n for i in p]
-fs_present = [1 - (1-i)**n for i in fs]
+# probability a permutation of i choose w elements is met (frequency f)
+def perm_match(w,f,i):
+    return (f**i)*((1-f)**(w-i))
 
-# results
 
-#misses2 = []
-#misses3 = []
+'''
+Class: Multicell Sequencing Analysis
+Function: Test for well properties of particular parameter sets
+'''
 
-results = []
-
-for f,f_orig in zip(fs_present,fs):
-    misses = []
-
-    for i in xrange(0,w+1): # cut out not appearing case and one appearance case (noise)
-        misses.append((1 - reduce(op.mul,[1 - ((f1**i)*((1-f1)**(w-i))) for f1 in p_present],1))*ncr(w,i)*(f**i)*((1-f)**(w-i)))
-        #misses2.append((1 - reduce(op.mul,[1 - ((f1**i)*((1-f1)**(w-i))) for f1 in p_present],1)))
-        #misses3.append(ncr(w,i)*(f**i)*((1-f)**(w-i)))
-        
-    #misses.append(ncr(w,0)*((1-f)**(w)))
-
-    print 'Added clone with frequency {}%: Failure rate of {}%'.format(round(100*f_orig,4),round(100*sum(misses),4))     
-    results.append(sum(misses))
+class Multicell_Sequencing:
+    def __init__(self,w=1000,n=1000):
+        self.w,self.n = w,n
+        self.p,self.fs = None,None
     
+    def build_repertoire(self,alpha=1,count=20000):
+        p = [(o+1)**-alpha for o in xrange(count)]
+        self.p = [i/sum(p) for i in p]
+
+    def fs_linspace(self,start,stop,num=5):
+        self.fs = np.linspace(start,stop,num)
+
+    def fs_logspace(self,start,stop,num=5):
+        self.fs = np.logspace(start,stop,num) 
+
+    def start_test(self,silent=True):
+        # check to make sure important sets are define
+        if self.p is None:
+            print 'Repertoire not defined!'
+            return None
+        elif self.fs is None:
+            print 'Clone frequencies not defined!'
+            return None 
+        
+        self.p_present = [1 - (1-i)**self.n for i in self.p]
+        self.fs_present = [1 - (1-i)**self.n for i in self.fs]
+
+        # prep for simulation
+        results = []
+        repertoire_perm_match = [(1 - reduce(op.mul,[1 - perm_match(self.w,f1,i) for f1 in self.p_present],1)) for i in xrange(0,self.w+1)] # p_present modification
+
+        # main simulation loop
+        for f,f_orig in zip(self.fs_present,self.fs):
+            binomial = binomial_list(self.w,f)
+            results.append(sum([a*b for a,b in zip(binomial,repertoire_perm_match)]))
+            if not silent: print 'Added clone with frequency {}%: Failure rate of {}%'.format(round(100*f_orig,4),round(100*results[-1],4))     
+
+        return self.fs,results 
+
 '''
-plt.plot(xrange(2,2+len(misses2)), misses2)
-plt.xlabel('Number of well appearances (k)')
-plt.ylabel('Probability of match occurring')
-plt.title('Match distribution for f = {} as a function of k'.format(f))
-print 'Probability of failure: {}'.format(sum(misses))
+Visualization methods (maybe put this into a class eventually
 '''
-plt.semilogx(fs,results)
-plt.xlabel('Frequency of target clone')
-plt.ylabel('Probability of convoluted signals')
-plt.title('Probability of convoluted signals for n = {}'.format(n))
-#'''
-plt.show()
+
+def visualize_1D(fs,results):
+    plt.semilogx(fs,results)
+    plt.xlabel('Frequency of target clone')
+    plt.ylabel('Probability of convoluted signals')
+    plt.show(block=False)
+    raw_input('Hit enter to close...')
+    plt.close()
+
+def visualize_repertoire(p):
+    plt.loglog(xrange(1,len(p)+1),p)
+    plt.title('Repertoire clonal frequencies')
+    plt.xlabel('Clone Index (#)')
+    plt.ylabel('Frequency of Clone (% total)')
+    plt.show(block=False)
+    raw_input('Hit enter to close...')
+    plt.close()
+
+'''
+Namespace main execution
+'''
+
+if __name__ == '__main__':
+    main()
+
+
